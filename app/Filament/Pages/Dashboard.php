f4677428bef8558a2c1597b4dd6e6effeafdb013
Phoenix-H22 <alkady2019@gmail.com>
2025-08-27 04:02:53 +0300

    Remove StatsOverview widget from dashboard
    Removed the StatsOverview widget from both the Filament Dashboard page and the AdminPanelProvider widget registration. Also updated README formatting and published Livewire assets to the public vendor directory.

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -2,17 +2,9 @@
 
 namespace App\Filament\Pages;
 
-use App\Filament\Widgets\StatsOverview;
 use Filament\Pages\Dashboard as BaseDashboard;
 
 class Dashboard extends BaseDashboard
 {
     protected static ?string $navigationIcon = 'heroicon-o-home';
-
-    protected function getHeaderWidgets(): array
-    {
-        return [
-            StatsOverview::class,
-        ];
-    }
 }
